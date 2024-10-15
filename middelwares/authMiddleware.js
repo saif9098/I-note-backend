e009ecf9b2fe217e1335650f94481f4bfb24aa9e
Mo. Saif raja <126969644+saif9098@users.noreply.github.com>
2024-10-16 00:39:53 +0530

    Update authMiddleware.js

--- middelwares/authMiddleware.js
+++ middelwares/authMiddleware.js
@@ -1,17 +1,28 @@
 import JWT from "jsonwebtoken";
 
 const userAuth = async (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith("Bearer")) {
-    next("Auth Failed : No Authorization Header");
-  }
-  const token = authHeader.split(" ")[1];
   try {
-    const payload = JWT.verify(token, process.env.JWT_SECRET);
-    req.body.user = { userId: payload.userId };
+    const authHeader = req.headers.authorization;
+    
+    // Check if authorization header is missing or improperly formatted
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).send({
+        success: false,
+        message: "UnAuthorized Access",
+      });
+    }
+
+    // Extract token from "Bearer <token>"
+    const token = authHeader.split(" ")[1];
+
+    // Verify token
+    const decode = JWT.verify(token, process.env.JWT_SECRET);
+    req.user = decode;
     next();
   } catch (error) {
-    next("Auth Failed",error);
+    console.log(error);
+
+  
   }
 };
 
