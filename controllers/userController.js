9bddfb1993861391cba26576c5826669e821028f
Mo. Saif raja <126969644+saif9098@users.noreply.github.com>
2024-10-16 00:42:31 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,26 +1,26 @@
 import userModel from "../models/userModel.js";
 
 export const updateUserController = async (req, res, next) => {
-  const { _id,nName, nEmail, nLastName,nLocation} = req.body;
-  if (!nName || !nEmail || !nLastName || !nLocation) {
-    next("Please Provide All Fields");
+  const { nName, nEmail, nLastName,nPassword} = req.body;
+  const userId = req.user.userId; 
+  if (!nName || !nEmail || !nLastName || !nPassword) {
+    return res.status(404).send({
+      success: false,
+      message: "Please provide all fields",
+    });
   }
-  const user = await userModel.findOne({ _id: _id });
-  user.name = nName;
-  user.lastName = nLastName;
-  user.email = nEmail;
-  user.location = nLocation;
+  const user = await userModel.findOne({ _id: userId});
+  const newPassword = nPassword ? nPassword : undefined;
   const updatedUser = await userModel.findByIdAndUpdate(
-    _id,
+  userId,
     {
       name: nName || user.name,
       lastName: nLastName || user.lastName,
       email: nEmail || user.email,
-      location: nLocation || user.location,
+      password: newPassword || user.password,
     },
     { new: true }
   );
-  await user.save();
   const token = user.createJWT();
   res.status(200).json({
     user,
@@ -29,28 +29,3 @@ export const updateUserController = async (req, res, next) => {
   });
 };
 
-// get user data
-export const getUserController = async (req, res, next) => {
-  try {
-    const user = await userModel.findById({ _id: req.body.user.userId });
-    user.password = undefined;
-    if (!user) {
-      return res.status(200).send({
-        message: "User Not Found",
-        success: false,
-      });
-    } else {
-      res.status(200).send({
-        success: true,
-        data: user,
-      });
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({
-      message: "auth error",
-      success: false,
-      error: error.message,
-    });
-  }
-};
