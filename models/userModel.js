cb154535a9714832290adb4179b2a274a913ff1a
Mo. Saif raja <126969644+saif9098@users.noreply.github.com>
2024-10-16 00:38:47 +0530

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -24,10 +24,7 @@ const userSchema = new mongoose.Schema(
       minlength: [6, "Password length should be greater than 6 character"],
       select: true,
     },
-    location: {
-      type: String,
-      default: "India",
-    },
+   
   },
   { timestamps: true }
 );
@@ -37,7 +34,18 @@ userSchema.pre("save", async function () {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+userSchema.pre("findOneAndUpdate", async function (next) {
+  // Get the update object
+  const update = this.getUpdate();
 
+  // Check if the password is being updated
+  if (update.password) {
+    const salt = await bcrypt.genSalt(10);
+    update.password = await bcrypt.hash(update.password, salt);
+  }
+
+  next();
+});
 //compare password
 userSchema.methods.comparePassword = async function (userPassword) {
   const isMatch = await bcrypt.compare(userPassword, this.password);
